7f6eeeb92fc956f1faa3087e82412dbd180c2797
Fernando Kuteken <fskuteken@gmail.com>
2019-08-21 00:36:04 -0300

    Implement addition functions for Vector2 classAdd #add, #addScalar, #addValues and #addVectors functions

--- src/math/vector2/vector2.js
+++ src/math/vector2/vector2.js
@@ -17,6 +17,34 @@ export default class Vector2 {
 
     return this;
   }
+
+  add(vector) {
+    this.x += vector.x;
+    this.y += vector.y;
+
+    return this;
+  }
+
+  addScalar(s) {
+    this.x += s;
+    this.y += s;
+
+    return this;
+  }
+
+  addValues(x, y) {
+    this.x += x;
+    this.y += y;
+
+    return this;
+  }
+
+  addVectors(u, v) {
+    this.x = u.x + v.x;
+    this.y = u.y + v.y;
+
+    return this;
+  }
 }
 
 Vector2.zero = new Vector2(0, 0);
