3c6a25ba0e25f8c7b72811b3c1de186d84852cf9
Fernando Kuteken <fskuteken@gmail.com>
2019-08-24 03:01:54 -0300

    Add JSDoc comments for Matrix3 class

--- src/math/matrix3.js
+++ src/math/matrix3.js
@@ -1,4 +1,11 @@
+/**
+ * A 3x3 matrix.
+ * Its values are stored in column-major order.
+ */
 export default class Matrix3 {
+  /**
+   * Creates a new identity Matrix3.
+   */
   constructor() {
     this.values = [
       1, 0, 0,
@@ -7,6 +14,19 @@ export default class Matrix3 {
     ];
   }
 
+  /**
+   * Creates a new Matrix3 from the values of each element.
+   * @param {Number} a00 The element at row 0 and column 0.
+   * @param {Number} a01 The element at row 0 and column 1.
+   * @param {Number} a02 The element at row 0 and column 2.
+   * @param {Number} a10 The element at row 1 and column 0.
+   * @param {Number} a11 The element at row 1 and column 1.
+   * @param {Number} a12 The element at row 1 and column 2.
+   * @param {Number} a20 The element at row 2 and column 0.
+   * @param {Number} a21 The element at row 2 and column 1.
+   * @param {Number} a22 The element at row 2 and column 2.
+   * @returns {Matrix3} The created matrix.
+   */
   static fromValues(
     a00, a01, a02,
     a10, a11, a12,
@@ -22,6 +42,19 @@ export default class Matrix3 {
     return matrix;
   }
 
+  /**
+   * Sets the elements of the matrix.
+   * @param {Number} a00 The element at row 0 and column 0.
+   * @param {Number} a01 The element at row 0 and column 1.
+   * @param {Number} a02 The element at row 0 and column 2.
+   * @param {Number} a10 The element at row 1 and column 0.
+   * @param {Number} a11 The element at row 1 and column 1.
+   * @param {Number} a12 The element at row 1 and column 2.
+   * @param {Number} a20 The element at row 2 and column 0.
+   * @param {Number} a21 The element at row 2 and column 1.
+   * @param {Number} a22 The element at row 2 and column 2.
+   * @returns {Matrix3} The matrix.
+   */
   set(
     a00, a01, a02,
     a10, a11, a12,
@@ -36,6 +69,11 @@ export default class Matrix3 {
     return this;
   }
 
+  /**
+   * Copies the elements from another matrix.
+   * @param {Matrix3} matrix The matrix to copy the elements from.
+   * @returns {Matrix3} The matrix.
+   */
   copy(matrix) {
     const v = this.values;
 
@@ -49,12 +87,18 @@ export default class Matrix3 {
   }
 }
 
+/**
+ * An identity Matrix3.
+ */
 Matrix3.identity = Matrix3.fromValues(
   1, 0, 0,
   0, 1, 0,
   0, 0, 1,
 );
 
+/**
+ * A Matrix3 with all elements equal to zero.
+ */
 Matrix3.zero = Matrix3.fromValues(
   0, 0, 0,
   0, 0, 0,
