6b74cbfc7f6c893e62c999243158192560496290
Fernando Kuteken <fskuteken@gmail.com>
2019-08-23 23:51:26 -0300

    Export matrix classes from math/index

--- src/math/index.js
+++ src/math/index.js
@@ -1,8 +1,12 @@
+import Matrix3 from './matrix3';
+import Matrix4 from './matrix4';
 import Vector2 from './vector2';
 import Vector3 from './vector3';
 import Vector4 from './vector4';
 
 export default {
+  Matrix3,
+  Matrix4,
   Vector2,
   Vector3,
   Vector4,
--- tests/math/index.test.js
+++ tests/math/index.test.js
@@ -1,9 +1,19 @@
 import Math from '../../src/math/index';
+import Matrix3 from '../../src/math/matrix3';
+import Matrix4 from '../../src/math/matrix4';
 import Vector2 from '../../src/math/vector2';
 import Vector3 from '../../src/math/vector3';
 import Vector4 from '../../src/math/vector4';
 
 describe('Math', () => {
+  it('exports the Matrix3 class', () => {
+    expect(Math.Matrix3).toEqual(Matrix3);
+  });
+
+  it('exports the Matrix4 class', () => {
+    expect(Math.Matrix4).toEqual(Matrix4);
+  });
+
   it('exports the Vector2 class', () => {
     expect(Math.Vector2).toEqual(Vector2);
   });
