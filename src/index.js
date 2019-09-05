0ef660eb11c9041d7be992198d44d71be09d85ca
Fernando Kuteken <fskuteken@gmail.com>
2019-09-05 00:26:29 -0300

    Export recently created classes from src/index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+import Camera3 from './camera3';
+import Matrix2 from './matrix2';
 import Matrix3 from './matrix3';
 import Matrix4 from './matrix4';
 import Quaternion from './quaternion';
+import Ray3 from './ray3';
+import Size2 from './size2';
+import Size3 from './size3';
 import Sphere from './sphere';
 import Vector2 from './vector2';
 import Vector3 from './vector3';
 import Vector4 from './vector4';
 
 export default {
+  Camera3,
+  Matrix2,
   Matrix3,
   Matrix4,
   Quaternion,
+  Ray3,
+  Size2,
+  Size3,
   Sphere,
   Vector2,
   Vector3,
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,26 @@
 import index from '../src/index';
+import Camera3 from '../src/camera3';
+import Matrix2 from '../src/matrix2';
 import Matrix3 from '../src/matrix3';
 import Matrix4 from '../src/matrix4';
 import Quaternion from '../src/quaternion';
+import Ray3 from '../src/ray3';
+import Size2 from '../src/size2';
+import Size3 from '../src/size3';
 import Sphere from '../src/sphere';
 import Vector2 from '../src/vector2';
 import Vector3 from '../src/vector3';
 import Vector4 from '../src/vector4';
 
 describe('index', () => {
+  it('exports the Camera3 class', () => {
+    expect(index.Camera3).toEqual(Camera3);
+  });
+
+  it('exports the Matrix2 class', () => {
+    expect(index.Matrix2).toEqual(Matrix2);
+  });
+
   it('exports the Matrix3 class', () => {
     expect(index.Matrix3).toEqual(Matrix3);
   });
@@ -18,6 +31,18 @@ describe('index', () => {
 
   it('exports the Quaternion class', () => {
     expect(index.Quaternion).toEqual(Quaternion);
+  });
+
+  it('exports the Ray3 class', () => {
+    expect(index.Ray3).toEqual(Ray3);
+  });
+
+  it('exports the Size2 class', () => {
+    expect(index.Size2).toEqual(Size2);
+  });
+
+  it('exports the Size3 class', () => {
+    expect(index.Size3).toEqual(Size3);
   });
 
   it('exports the Sphere class', () => {
